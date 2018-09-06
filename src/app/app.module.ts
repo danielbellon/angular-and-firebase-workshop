e672d82c4b55d7e80aeb86db2f796fd4f0f7d99a
Daniel Felipe Bellón <danielbellon77@gmail.com>
2018-09-06 17:05:01 -0500

    initial commit for the angular and firebase workshop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { ChatComponent } from './chat/chat.component';
+import { MessageComponent } from './message/message.component';
+import { InputComponent } from './input/input.component';
+
+const config = {
+  apiKey: 'AIzaSyBjaWg8xgzho5V5roxwp3KMFwyVUd4O9ws',
+  authDomain: 'angular-and-firebase-53804.firebaseapp.com',
+  databaseURL: 'https://angular-and-firebase-53804.firebaseio.com',
+  projectId: 'angular-and-firebase-53804',
+  storageBucket: 'angular-and-firebase-53804.appspot.com',
+  messagingSenderId: '449324116038'
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    MessageComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,36 @@
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { Message } from '../message.model';
+import { Component, OnInit, Input, AfterViewChecked, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  @ViewChild('msgContainer') private messagesContainer: ElementRef;
+  @Input() userAuth: string;
+  messages: Observable<any[]>;
+  private msgRef: AngularFirestoreCollection<Message>;
+
+  constructor(private db: AngularFirestore) {
+    this.msgRef = db.collection<Message>('messages', ref => ref.orderBy('timestamp'));
+  }
+
+  ngOnInit() {
+    this.messages = this.msgRef.valueChanges();
+    this.ngAfterViewChecked();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+}
